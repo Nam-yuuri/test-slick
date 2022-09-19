f3c8f7ace5cc02d5765b1a7cad400e61f21a014a
Nam-yuuri <1851061325@e.tlu.edu.vn>
2022-09-19 16:24:33 +0700

    upload code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 import './App.css';
+import { dataDigital } from './data';
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Slider {...settings}>
+          {dataDigital.map(item=>(
+            <div className='card'>
+            <div className='card-top'>
+              <img src={item.linkImg} alt={item.title} />
+              <h1>{item.title}</h1>
+            </div>
+            <div className='card-bottom'>
+              <h3>{item.price}</h3>
+              <p>{item.category}</p>
+            </div>
+          </div>
+          ))}
+        </Slider>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const dataDigital =[
+    {
+        id: 1,
+        title: 'nam',
+        price: '$123',
+        category: 'namsd',
+        linkImg: 'https://tse3.explicit.bing.net/th?id=OIP.nmY2o1MdUEMRBhFkRVxkyAHaGF&pid=Api&P=0'
+    },
+    {
+        id: 2,
+        title: 'nam',
+        price: '$123',
+        category: 'namsd',
+        linkImg: 'https://tse3.explicit.bing.net/th?id=OIP.nmY2o1MdUEMRBhFkRVxkyAHaGF&pid=Api&P=0'
+    },
+    {
+        id: 3,
+        title: 'nam',
+        price: '$123',
+        category: 'namsd',
+        linkImg: 'https://tse3.explicit.bing.net/th?id=OIP.nmY2o1MdUEMRBhFkRVxkyAHaGF&pid=Api&P=0'
+    },
+    {
+        id: 4,
+        title: 'nam',
+        price: '$123',
+        category: 'namsd',
+        linkImg: 'https://tse3.explicit.bing.net/th?id=OIP.nmY2o1MdUEMRBhFkRVxkyAHaGF&pid=Api&P=0'
+    },
+    {
+        id: 5,
+        title: 'nam',
+        price: '$123',
+        category: 'namsd',
+        linkImg: 'https://tse3.explicit.bing.net/th?id=OIP.nmY2o1MdUEMRBhFkRVxkyAHaGF&pid=Api&P=0'
+    },
+    {
+        id: 6,
+        title: 'nam',
+        price: '$123',
+        category: 'namsd',
+        linkImg: 'https://tse3.explicit.bing.net/th?id=OIP.nmY2o1MdUEMRBhFkRVxkyAHaGF&pid=Api&P=0'
+    },
+]
